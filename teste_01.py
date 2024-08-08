e24c2226b78161629ff7851067e2f0a81ece6720
vilson14 <31911012+vilson14@users.noreply.github.com>
2024-08-07 22:17:57 -0300

    Update teste_01.py

--- teste_01.py
+++ teste_01.py
@@ -1,4 +1,3 @@
-
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import plotly
@@ -15,8 +14,53 @@ from datetime import date
 import streamlit as st
 import plotly.express as px
 
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
 #RUN - NAS PASTA C:\app
 #streamlit run .\teste_02.py
+
+
+
+
+
+url = 'https://br.investing.com/equities/vale-s.a.--americ'
+
+head = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+  'Accept-Encoding': 'none',
+  'Accept-Language': 'en-US,en;q=0.8',
+  'Connection': 'keep-alive',
+  'refere': 'https://br.investing.com/equities/vale-s.a.--americ',
+  'cookie': """your cookie value ( you can get that from your web page) """
+}
+
+
+
+#VIX-------------------------------------------------------------------------------------------------------------------------------------
+url = 'https://br.investing.com/indices/volatility-s-p-500'
+req = Request(url, headers=head)
+html1 = urlopen(req)
+html2 = html1.read()
+soup = BeautifulSoup(html2, "html.parser")
+
+vix_preco_01 = soup.find('div', {"data-test": "instrument-price-last"})
+vix_preco_02 = vix_preco_01.getText()
+vix_preco_03 = vix_preco_02.replace(',', '.')
+vix_preco_04 = float(vix_preco_03)
+
+
+vix_var_01 = soup.find('span', {"data-test": "instrument-price-change-percent"})
+vix_var_02 = vix_var_01.getText()
+vix_var_03 = vix_var_02.replace('(', '')
+vix_var_04 = vix_var_03.replace(')', '')
+vix_var_05 = vix_var_04.replace('%', '')
+vix_var_06 = vix_var_05.replace(',', '.')
+vix_var_07 = float(vix_var_06)
+
+
 
 
 fig = make_subplots(rows=5, cols=4, subplot_titles=("VIX", "OURO", "BRENT", "SP500", "WTI", "MINÉRIO DALIAN", "MINÉRIO CME", "EWZ", "SALDO TOTAL", "VALE", "PETRO", "SALDO - mat. básicos", "ITAU", "BRADESCO", "BRASIL", "SALDO - financeiro"),
@@ -28,7 +72,7 @@ fig = make_subplots(rows=5, cols=4, subplot_titles=("VIX", "OURO", "BRENT", "SP5
                     horizontal_spacing = 0.05, vertical_spacing = 0.05)
 
 
-st.subheader("PRE-MARKET - MACRO + ADRS")
+st.subheader("PRE-MARKET - MACRO + ADRS " + vix_var_06)
 #st.dataframe(iris_df)
 
 
